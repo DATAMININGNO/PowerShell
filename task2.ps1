c30326ac5f45c1a45cbf1a1c77b47377ac1c4be9
Fsalm <73988987+DATAMININGNO@users.noreply.github.com>
2021-02-24 15:42:09 +0100

    Update task2.ps1

--- task2.ps1
+++ task2.ps1
@@ -2,36 +2,11 @@
 
 ==TASKS==
 
-1. The script below contains a bug, tagged with a FIXME comment in
-the code. Fix it, changing as little code as necessary. Save the
-result as task1.ps1.
 
 2. With the result from task 1, refactor the code to make it easier to read,
 maintain, and extend. You may also rewrite the script using a different
 language or framework if you are feeling creative. Save the result
 as task2.<whatever>
-
-3. With the result from task 2 as your starting point, implement the
-feature specified below. Save the result as task3.ps1 (or
-task3.<whatever> if you use another language).
-
-Specification: There should be a finite amount of servings for each
- dish on the menu, i.e. 15 Dagens, 2 Vegetar, 5 Halal. Requesting a
- dish that has no servings left should return the message "Sorry, no
- more left of <dish name> today."
-
-Example run:
-What would you like today? (type 'how is business?' to attempt small talk, or 'not hungry' to stop)
-Input: Vegetar
-Vegetar, here you go.
-What would you like today? (type 'how is business?' to attempt small talk, or 'not hungry' to stop)
-Input: Vegetar
-Vegetar, here you go.
-What would you like today? (type 'how is business?' to attempt small talk, or 'not hungry' to stop)
-Input: Vegetar
-Sorry, no more left of <Vegetar> today.
-What would you like today? (type 'how is business?' to attempt small talk, or 'not hungry' to stop)
-Input: [...]
 
 #>
 cls
